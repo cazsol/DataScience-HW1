4713c6863dcb91c70c79c1137d0bf12a95e728d0
k <theoneas1@gmail.com>
2017-10-26 18:33:29 -0400

    loop implemented for request

--- HW1.py
+++ HW1.py
@@ -1,7 +1,10 @@
 import requests
 import matplotlib as pp
 import numpy as np
-from BS4 import BeautifulSoup
-url = "https://www.netmarketshare.com/operating-system-market-share.aspx?qprid=9&qpcustomb=1&qpsp=2007&qpnp=1&qptimeframe=Y"
-H = requests.get(url)
-soup = BeautifulSoup(H.text)
+from bs4 import BeautifulSoup
+lowerBound = 2007
+higherBound = 2017
+for i in range(lowerBound,higherBound+1):
+    url = 'https://www.netmarketshare.com/operating-system-market-share.aspx?qprid=9&qpcustomb=1&qpsp={0}&qpnp=1&qptimeframe=Y'.format(i)
+    H = requests.get(url, verify=False)
+    soup = BeautifulSoup(H.text)
